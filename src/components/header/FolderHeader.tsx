f3fdcc9026dedc340843076d54c84c093c3093f4
ohddang <ohddang509@gmail.com>
2024-02-08 11:06:10 +0900

    feat: intersectionObserver 링크 추가 input

--- src/components/header/FolderHeader.tsx
+++ src/components/header/FolderHeader.tsx
@@ -1,6 +1,6 @@
 import "./header.css";
 
-import { useState, useRef, MouseEvent } from "react";
+import { useState, useRef, useEffect } from "react";
 import ModalPortal from "components/modal/ModalPortal";
 import { BaseModal, ModalType } from "../modal";
 
@@ -8,8 +8,9 @@ const FolderHeader = () => {
   const [showModal, setShowModal] = useState(false);
   const [modalType, setModalType] = useState(0);
   const linkRef = useRef<HTMLInputElement>(null);
+  const titleRef = useRef<HTMLDivElement>(null);
 
-  const onShowModal = (event: MouseEvent<HTMLDivElement | MouseEvent>, modalType: number) => {
+  const onShowModal = (event: React.MouseEvent<HTMLDivElement | MouseEvent>, modalType: number) => {
     event.stopPropagation();
 
     if (linkRef.current && linkRef.current.value === "") {
@@ -25,10 +26,49 @@ const FolderHeader = () => {
     setShowModal(false);
   };
 
+  const io = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (titleRef.current) {
+        if (entry.target.className === "title_container") {
+          if (entry.intersectionRatio <= 0) {
+            titleRef.current.classList.add("move_to_bottom");
+          }
+        } else if (entry.target.className === "footer_list") {
+          if (entry.intersectionRatio > 0) {
+            titleRef.current.classList.add("move_to_bottom", "display_none");
+          } else {
+            titleRef.current.classList.remove("display_none");
+          }
+        } else if (entry.target.className === "navigation_container") {
+          if (entry.intersectionRatio > 0) {
+            titleRef.current.classList.remove("move_to_bottom");
+          }
+        }
+      }
+    });
+  }, {});
+
+  useEffect(() => {
+    const title = document.querySelector(".title_container");
+    if (title) {
+      io.observe(title);
+    }
+
+    const navi = document.querySelector(".navigation_container");
+    if (navi) {
+      io.observe(navi);
+    }
+
+    const footer = document.querySelector(".footer_list");
+    if (footer) {
+      io.observe(footer);
+    }
+  }, []);
+
   return (
     <>
       <section className="title_container">
-        <div className="folder_title_container">
+        <div className="folder_title_container" ref={titleRef}>
           <div className="folder_title_input_container">
             <img src="./images/link.svg" className="link_image" />
             <input ref={linkRef} className="folder_title_input" placeholder="링크를 추가해 보세요." />
